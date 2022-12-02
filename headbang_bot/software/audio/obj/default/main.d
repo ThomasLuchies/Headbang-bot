28b7a1daeed32b435f428fe1279bdc2e690cf184
Robin Goeree <56107687+RobinGoeree2@users.noreply.github.com>
2022-12-02 11:48:18 +0100

    Created c code for audio in eclipse

---
+++ headbang_bot/software/audio/obj/default/main.d
@@ -0,0 +1,15 @@
+obj/default/main.o: main.c \
+ c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\drivers\inc\altera_up_avalon_audio.h \
+ c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\alt_types.h \
+ c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\sys\alt_dev.h \
+ c:/Users/Robin/Documents/GitHub/Headbang-bot/headbang_bot/software/audio_bsp/system.h \
+ c:/Users/Robin/Documents/GitHub/Headbang-bot/headbang_bot/software/audio_bsp/linker.h \
+ c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\sys\alt_llist.h \
+ c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\priv\alt_dev_llist.h
+c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\drivers\inc\altera_up_avalon_audio.h:
+c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\alt_types.h:
+c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\sys\alt_dev.h:
+c:/Users/Robin/Documents/GitHub/Headbang-bot/headbang_bot/software/audio_bsp/system.h:
+c:/Users/Robin/Documents/GitHub/Headbang-bot/headbang_bot/software/audio_bsp/linker.h:
+c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\sys\alt_llist.h:
+c:\users\robin\documents\github\headbang-bot\headbang_bot\software\audio_bsp\hal\inc\priv\alt_dev_llist.h:
