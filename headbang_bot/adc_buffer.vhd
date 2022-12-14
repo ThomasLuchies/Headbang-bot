b37cdfc2c771bf40e7255538923388125601cdb2
Sydney Minnaar <sydneyminnaar2000@gmail.com>
2022-12-14 19:18:16 +0100

    tested adc_buffer

--- headbang_bot/adc_buffer.vhd
+++ headbang_bot/adc_buffer.vhd
@@ -7,10 +7,7 @@ entity adc_buffer is port
 	clk, data_in, reset: in std_logic;
 	first_channel_out: out std_logic_vector(15 downto 0);
 	last_channel_out: out std_logic_vector(15 downto 0);
-	channels_ready: out std_logic;
-	-- test pins
-	fill_channels_test: out std_logic;
-	channel_index_test: out std_logic_vector(4 downto 0)
+	channels_ready: out std_logic
 );
 end entity;
 
@@ -28,8 +25,6 @@ begin
 	begin
 		if rising_edge(clk) then
 			if reset = '1' then
-				first_channel <= (others => '0');
-				last_channel <= (others => '0');
 				channel_index <= 0;
 				fill_channels <= '1';
 				channels_ready <= '0';
@@ -40,7 +35,8 @@ begin
 					first_channel(channel_index) <= data_in;
 				elsif channel_index <= 31 then
 					last_channel(channel_index - 16) <= data_in;
-				elsif channel_index = 31 then
+				end if;
+				if channel_index = 31 then
 					fill_channels <= '0';
 					channels_ready <= '1';
 				end if;
@@ -50,10 +46,7 @@ begin
 		end if;
 	end process;
 	
-	first_channel_out <= first_channel when channel_index = 31 else (others => '0');
-	last_channel_out <= last_channel when channel_index = 31 else (others => '0');
-	-- test pins
-	fill_channels_test <= fill_channels;
-	channel_index_test <= std_logic_vector(to_unsigned(channel_index, channel_index_test'length));
+	first_channel_out <= first_channel when channel_index = 31;
+	last_channel_out <= last_channel when channel_index = 31;
 	
 end architecture;
